c1cced680c570b95e3f339a9fde97d3044bedd88
Daniel P.K <danilofletcher978@gmail.com>
2025-08-25 18:39:30 +0000

    auth logic | signup login

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,40 @@
-
 // auth controller functions to be called in the auth routes
+
+import User from "../models/user.model";
+import bcrypt from "bcryptjs"
+
 // these functions will handle the logic for signup, login, and logout, obviously   
-export const signup = (res, req) => {
-    res.send('singup route');
+export const signup = async (res, req) => {
+    const {fullName, email, password} = req.body;
+    try {
+        if (password.length < 6){
+            return res.status(400).json({message: "Pasword must be at least 6 characters" })
+        }
+
+        const user = await User.findOne({email})
+
+        if (user) return res.status(400).json({message: "Email already exsits"})
+
+        //hash password 
+        const salt = await bcrypt.genSalt(10) 
+        const hashedPassword = await bcrypt.hash(password, salt) 
+
+        const newUser = new User({
+            fullName,
+            email, 
+            password: hashedPassword,  
+        });
+
+        if (newUser){
+            //generate jwt token here   
+
+        }else{
+            res.status(400).json({message: "Invalid user data"});
+        }
+
+    } catch (error) {
+        
+    }
 }
 
 export const login = (res, req) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,9 @@ dotenv.config(); // Load environment variables from .env file
 const app = express();
 
 //Use environment varaibles from .env file. 
-const PORT = process.env.PORT || 5001; 
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json()); // Middleware to parse JSON request bodies | basically so I can fetch the json data.  
 
 app.use('/api/auth', authRoutes)
 
