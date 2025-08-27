7ae675f059d96904aff8e859c18246ad701895f8
Daniel P.K <danilofletcher978@gmail.com>
2025-08-27 13:35:38 +0000

    grabMsgBtnUsers

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
+//This would help me with the sidebar to get all users except the logged in user.  
 export const getUsersforSidebar = async(req, res) => {
     try {
         const loggedInUserId = req.user._id;
@@ -9,5 +11,22 @@ export const getUsersforSidebar = async(req, res) => {
     } catch (error) {
         console.log("Error in getUsersforSidebar controller:", error.message);
         res.status(500).json({message: "Internal Server error"});
+    }
+}
+
+//Get all messages between logged in user and the user he is chatting with.
+export const getMessages = async(req, res) => {
+    try {
+        const {id: userToChatId} = req.params;
+        const senderId = req.user._id;  
+        const messages = await Message.find({
+            $or: [
+                {senderId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: senderId}
+            ]
+        }).sort({createdAt: 1}); // Sort messages in ascending order based on creation time  
+    } catch (error) {
+        console.log("Error in getMessages controller:", error.message);
+        res.status(500).json({message: "Internal Server error"}); 
     }
 }
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,10 @@
 import express from 'express'; 
 import { protectRoute } from '../middleware/auth.middleware';  
-import { get } from 'mongoose';
+import { getUsersforSidebar, getMessages } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersforSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;  
